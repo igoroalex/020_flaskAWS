6cc9921804bdc2935fc455b6015f88e07ef0465f
GoRo <igoroalex@gmail.com>
2022-04-21 23:15:57 +0300

    Add checkout in CI_17

--- application.py
+++ application.py
@@ -1,47 +1,24 @@
 """
 Run App
 """
+from flask import Flask
 # from app.main import create_app
 #
-# from config import settings
-#
 # application = create_app()
+application = Flask(__name__)
+
+@application.route("/")
+def hello_world():
+    return "<p>Hello, World112!</p>"
 #
 # if __name__ == "__main__":
 #     application.run()
 #     # app.run(debug=settings.DEBUG, port=settings.PORT, host=settings.HOST)
 
-from flask import Flask
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
 
 # EB looks for an 'application' callable by default.
-application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-
-@application.route("/1/")
-def hello_world():
-    return "<p>Hello, World112!</p>"
 
 # run the app.
 if __name__ == "__main__":
