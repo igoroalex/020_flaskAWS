fdc9e3954631d4941879905a9531fad5329a15a1
GoRo <igoroalex@gmail.com>
2022-04-21 23:38:42 +0300

    Add checkout in CI_19

--- application.py
+++ application.py
@@ -2,29 +2,14 @@
 Run App
 """
 from flask import Flask
-# from app.main import create_app
-#
-# application = create_app()
+
 application = Flask(__name__)
 
 
 @application.route("/")
 def hello_world():
-    return "<p>Hello, World112!</p>"
+    return "<p>Hello, World 18 !</p>"
 
 
 if __name__ == "__main__":
     application.run()
-#     # app.run(debug=settings.DEBUG, port=settings.PORT, host=settings.HOST)
-
-
-# EB looks for an 'application' callable by default.
-
-
-
-# run the app.
-# if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    # application.debug = True
-    # application.run()
---
+++ tests/test_one.py
@@ -0,0 +1,6 @@
+def test_first():
+    assert True
+
+
+def test_second():
+    assert False != True
