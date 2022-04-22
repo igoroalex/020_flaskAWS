c4138bcf8ec30dc7b3df0ad4094b5b4d4e859adc
GoRo <igoroalex@gmail.com>
2022-04-22 10:40:17 +0300

    Add EB extensions_3

--- application.py
+++ application.py
@@ -3,13 +3,13 @@ Run App
 """
 from flask import Flask
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-@application.route("/")
+@app.route("/")
 def hello_world():
     return "<p>Hello, World 18 !</p>"
 
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
